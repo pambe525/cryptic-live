260106e5df5aafa13432ce2bcd307c57258ab8e1
Prashant Ambe <prashant.ambe@gmail.com>
2024-11-26 21:31:33 -0500

    Added a e2e test to HomePage to test root layout

--- __tests__/e2e/HomePage.test.js
+++ __tests__/e2e/HomePage.test.js
@@ -1,7 +1,23 @@
 describe('Home Page', () => {
 
-  it('should be titled "Google"', async () => {
+  beforeEach(async () => {
     await page.goto("http://localhost:3000/");
+  })
+
+  it('should have correct meta data', async () => {
     await expect(page.title()).resolves.toMatch('CrypticLive Home');
+    const description = await page.$eval("head > meta[name='description']", element => element.content);
+    expect(description).toMatch("Word Puzzles for Cruciverbalists");
   });
-});
+
+  it('should have root layout', async () => {
+    const header = await page.$(".container > header");
+    const footer = await page.$(".container > footer");
+    const navbar = await page.$(".container > nav");  // should not contain navbar
+    const sections = await page.$$(".container > section");
+    expect(header).not.toBeNull();
+    expect(footer).not.toBeNull();
+    expect(navbar).toBeNull();
+    expect(sections.length).toBe(2);  // Home page content
+  });
+})
