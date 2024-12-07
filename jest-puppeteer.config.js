87cfd816e05df41272e8e29431a4713deb03b198
Prashant Ambe <prashant.ambe@gmail.com>
2024-12-07 09:41:50 -0500

    Added jest-puppeteer config

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  launch: {
+    headless: true,
+    args: ['--no-sandbox']
+  },
+  server: {
+    command: 'npm start', // Command to start your local server
+    port: 3000, // Port your app is running on
+    launchTimeout: 20000 // Adjust if needed
+  }
+};
