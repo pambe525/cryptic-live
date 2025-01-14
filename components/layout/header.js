cc75814e886f7ddda41790292532d2cf3946570b
Prashant Ambe <prashant.ambe@gmail.com>
2025-01-14 15:18:52 -0500

    Refactor header component to import logo image from local path

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import styles from "./layout.module.css";
 import Navbar from "./navbar.js";
+import logo from "/public/logo.png";
 
 export default function Header() {
   return (
     <>
       <header className={styles.header}>
         <a href="/">
-          <Image src="/logo.png" alt="Logo" width={180} height={40} priority />
+          <Image src={logo} alt="Logo" width={180} height={40} priority />
         </a>
         <hr />
         <Navbar />
       </header>
-
     </>
   )
 }
