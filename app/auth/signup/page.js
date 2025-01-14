7a1e10521231a718ac4243a4a26b547c27207628
Prashant Ambe <prashant.ambe@gmail.com>
2025-01-14 15:19:12 -0500

    Refactor SignUpPage to use InputField components for form inputs

--- app/auth/signup/page.js
+++ app/auth/signup/page.js
@@ -1,29 +1,20 @@
+import InputField from "@/components/forms/inputField.js";
+
 export default function SignUpPage() {
   return (
     <section className="darkMode fitToContent">
       <h2 className="fancyText">Sign Up</h2>
       <form>
+        <InputField labelText="Email" hint="Enter your active email for important updates!" type="email" id="email1" required />
+        <InputField labelText="Confirm Email" type="email" id="email2" required />
+        <InputField labelText="Password" hint="Minimum 8 characters" id="password1" minLength="8" required />
+        <InputField labelText="Confirm Password" id="password2" minLength="8" required />
+        <InputField labelText="Screen Name" hint="Unique name upto 16 alphanumeric chars" pattern="^[a-zA-Z0-9_]*$" id="screenName" maxLength="16" required />
         <p>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" size="30" required />
-        </p>
-
-        <p>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" minLength="6" required />
-          <span className="noteText">Minimum 8 characters</span>
-        </p>
-
-        <p>
-          <label htmlFor="screenName">Screen Name</label>
-          <input type="text" name="screenName" pattern="^[a-zA-Z0-9_]*$" maxLength="16" required />
-          <span className="noteText">Must be unique upto 16 alphanumeric chars</span>
-        </p>
-
-        <p className="vCentered">
           <button type="submit" className="fancyButton">Create Account</button>
           <a href="/auth/login" className="lightText toRight">Have an Account?</a>
         </p>
+        <p>* required field</p>
       </form>
     </section>
   )
