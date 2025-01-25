98cd51131547f575e3c96a074f645bd9f9cafa4b
Prashant Ambe <prashant.ambe@gmail.com>
2025-01-25 11:59:18 -0500

    Refactor SignUpPage to include email verification note and update form submission logic

--- app/auth/signup/page.js
+++ app/auth/signup/page.js
@@ -1,15 +1,22 @@
 import InputField from "@/components/forms/inputField.js";
+import { signIn } from "@/auth.js";
 
 export default function SignUpPage() {
+  const emailNote = "An email will be sent for verification. Click on the link in the email to complete the registration process.";
   return (
-    <section className="darkMode fitToContent">
+    <section className="darkMode">
       <h2 className="fancyText">Sign Up</h2>
-      <form>
-        <InputField labelText="Email" hint="Enter your active email for important updates!" type="email" id="email1" required />
-        <InputField labelText="Confirm Email" type="email" id="email2" required />
+      <form
+        action={async (formData) => {
+          "use server"
+          await signIn("resend", formData)
+        }}
+      >
+        <InputField labelText="Email" hint={emailNote} type="email" id="email" required />
+        {/* <InputField labelText="Confirm Email" type="email" id="email2" required />
         <InputField labelText="Password" type="password" hint="Minimum 8 characters" id="password1" minLength="8" required />
         <InputField labelText="Confirm Password" type="password" id="password2" minLength="8" required />
-        <InputField labelText="Screen Name" hint="Unique name upto 16 alphanumeric chars" pattern="^[a-zA-Z0-9_]*$" id="screenName" maxLength="16" required />
+        <InputField labelText="Screen Name" hint="Unique name upto 16 alphanumeric chars" pattern="^[a-zA-Z0-9_]*$" id="screenName" maxLength="16" required /> */}
         <p>
           <button type="submit" className="fancyButton">Create Account</button>
           <a href="/auth/login" className="lightText toRight">Have an Account?</a>
