f262764dc0c0303d5c4f34f158266a19a5f1c04a
Prashant Ambe <prashant.ambe@gmail.com>
2025-01-27 10:29:12 -0500

    Add email provider to NextAuth setup and update Prisma migrations for user model

--- auth.js
+++ auth.js
@@ -1,10 +1,22 @@
 import NextAuth from "next-auth";
-import Resend from "next-auth/providers/resend";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma/prisma.js";
-
+import { EmailProvider } from "next-auth/providers/email";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Resend],
+  providers: [
+    EmailProvider({
+      id: "email",
+      name: "Email",
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.AUTH_RESEND_FROM,
+    })],
 })
