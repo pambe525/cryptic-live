3e4df42f7bb8874b02cc0b88f9947d4ea386a173
Prashant Ambe <prashant.ambe@gmail.com>
2025-01-29 20:27:15 -0500

    Add Resend provider configuration to NextAuth and refactor auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,14 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma/prisma.js";
-import { EmailProvider } from "next-auth/providers/email";
+import authConfig from "./auth.config.js";
+
+// Debugging logs
+console.log("authConfig:", authConfig);
+//console.log("prisma:", prisma);
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  session: { strategy: "jwt" },
   adapter: PrismaAdapter(prisma),
-  providers: [
-    EmailProvider({
-      id: "email",
-      name: "Email",
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.AUTH_RESEND_FROM,
-    })],
+  ...authConfig,
 })
