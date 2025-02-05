c32341a8f3b617c013a5d8ffa88554ca40e59110
pambe <prashant.ambe@gmail.com>
2025-02-04 21:49:00 -0500

    Refactor authentication setup by removing auth.config.js and integrating Resend provider directly in auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,15 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma/prisma.js";
-import authConfig from "./auth.config.js";
-
-// Debugging logs
-console.log("authConfig:", authConfig);
-//console.log("prisma:", prisma);
+import Resend from "next-auth/providers/resend";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: { strategy: "jwt" },
   adapter: PrismaAdapter(prisma),
-  ...authConfig,
+  providers: [
+    Resend({
+      apiKey: process.env.AUTH_RESEND_KEY,
+      from: "no-reply@crypticlive.net"
+    }),
+  ],
 })
